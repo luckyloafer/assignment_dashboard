5e77a9e26a5a682021abd6ba8098b7336c411c73
patnana lakshmana murthy <patnanalakshmanamurthy9@gmail.com>
2024-01-09 09:31:27 +0530

    small fixes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className='dashboard'>
       <div className='dashboard-sections'> 
-      <text style={{fontSize:20}}>NOTE: we can update custom logos after hosting in AWS</text><br/>
+      <text style={{fontSize:20,color:"red",fontWeight:"bold"}}>NOTE: we can update custom logos after hosting in AWS</text><br/>
         <UpdateNavLogo />
         <br/>
         <UpdateHeaderBtnTxt /><br/>
